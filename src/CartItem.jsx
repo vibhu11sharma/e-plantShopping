9314eb3a4fc650dc70d3131c2ec24e66674be97f
Vibhu <vibhus417@gmail.com>
2026-07-07 06:02:58 -0400

    Complete cart functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,51 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
-  };
+  return cartItems.reduce((total, item) => {
+    return total + parseFloat(item.cost.substring(1)) * item.quantity;
+  }, 0);
+};
 
   const handleContinueShopping = (e) => {
-   
-  };
+  onContinueShopping(e);
+};
+
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
 
 
   const handleIncrement = (item) => {
-  };
+  dispatch(
+    updateQuantity({
+      name: item.name,
+      amount: item.quantity + 1,
+    })
+  );
+};
 
   const handleDecrement = (item) => {
-   
-  };
+  if (item.quantity > 1) {
+    dispatch(
+      updateQuantity({
+        name: item.name,
+        amount: item.quantity - 1,
+      })
+    );
+  } else {
+    dispatch(removeItem(item.name));
+  }
+};
 
   const handleRemove = (item) => {
-  };
+  dispatch(removeItem(item.name));
+};
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-  };
+  return parseFloat(item.cost.substring(1)) * item.quantity;
+};
 
   return (
     <div className="cart-container">
